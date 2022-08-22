c6df79ecce1e22fbb0a357a681aa4f9663fce520
Artem K <tem-k@ya.ru>
2022-08-22 10:15:11 +0700

    first commit

---
+++ задача 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Task_1
+{
+    static void Main()
+    {
+        int a, b, count;
+
+        Console.Write("A: ");
+        a = Int32.Parse(Console.ReadLine());
+        Console.Write("B: ");
+        b = Int32.Parse(Console.ReadLine());
+
+        if (a >= b)
+        {
+            Console.Write("you enter a >= b");
+            return;
+        }
+
+        for (int i = a; i <= b; i++)
+        {
+            Console.Write("{0} ", i);
+        }
+
+        count = b - a + 1;
+        Console.Write("\ncount: {0} ", count);
+    }
+}
---
+++ задача 3/Program.cs
@@ -0,0 +1,19 @@
+﻿static void Main()
+{
+    int size = 20;
+    int[] array = new int[20];
+    Random myRandom = new Random();
+
+    Console.WriteLine("Вывод с помощью for");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = myRandom.Next(0, 20);
+        Console.Write("{0} ", array[i]);
+    }
+
+    Console.WriteLine("\n\nВывод с помощью foreach");
+    foreach (var elem in array)
+    {
+        Console.Write("{0} ", elem);
+    }
+}
